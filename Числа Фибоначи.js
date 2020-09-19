7cb7176e107dd746b130f3916fbf49eb9aba39fc
DmitryG3D <63233357+DmitryG3D@users.noreply.github.com>
2020-09-19 10:03:01 +0300

    Update Числа Фибоначи.js

--- Числа Фибоначи.js
+++ Числа Фибоначи.js
@@ -1,15 +1,14 @@
-let array = [8, 1, 5, 6, 3, 7, 9, 2, 4];
-
-function sort(arr) {
-  for(let n = 0; n < arr.length; n++) {
-      for(let i = 0; i < arr.length - 1 - n; i++) {
-        if(arr[i] > arr[i + 1]) {
-          const buff = array[i]
-          arr[i] = arr[i + 1]
-          arr[i + 1] = buff
-        }
-      }
-    }
-    return arr;
+function fibonacci( number ) {
+  let a = 1;
+  let b = 1;
+  
+  for ( let i = 3; i <= number; i++ ) {
+    
+    let c = a + b;
+    a = b;
+    b = c;
   }
-alert(sort(array));
+  
+  return b;
+}
+alert(fibonacci(prompt("Write number:")));
